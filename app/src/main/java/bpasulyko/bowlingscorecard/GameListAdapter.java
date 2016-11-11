948df63ffb4f1596803ca79d29ac9b54e0f84fd1
bpasulyko <bpasulyko@gmail.com>
2016-11-11 14:59:35 -0600

    Moved remove icons to the left when delete mode is activated

--- app/src/main/java/bpasulyko/bowlingscorecard/GameListAdapter.java
+++ app/src/main/java/bpasulyko/bowlingscorecard/GameListAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -43,11 +42,10 @@ public class GameListAdapter extends BaseAdapter {
         if(convertView==null)
             vi = inflater.inflate(R.layout.game_row, null);
 
+        Game game = games.get(position);
         TextView heading = (TextView)vi.findViewById(R.id.game_date_list_item);
         TextView subHeading = (TextView)vi.findViewById(R.id.scores_sub_item);
-        ImageView removeIcon = (ImageView) vi.findViewById(R.id.remove_icon);
-
-        Game game = games.get(position);
+        ViewGroup removeIconView = (ViewGroup) vi.findViewById(R.id.remove_icon);
 
         heading.setText(game.getFormattedDateString() + ":  " + game.getScoresString());
         DecimalFormat df = game.getDecimalFormat();
@@ -59,8 +57,12 @@ public class GameListAdapter extends BaseAdapter {
         String average = df.format(Math.floor(threeGameTotal / 3));
         String runningAvg = df.format(game.getAverage());
         subHeading.setText(String.format("Total: %s  Average: %s  Running Avg: %s", total, average, runningAvg));
+
         if (visible) {
-            removeIcon.setVisibility(View.VISIBLE);
+            int left = (int) (50 * vi.getResources().getDisplayMetrics().density);
+            ViewGroup gameDetailsView = (ViewGroup) vi.findViewById(R.id.game_row);
+            gameDetailsView.setPadding(left, 0, 0, 0);
+            removeIconView.setVisibility(View.VISIBLE);
         }
         return vi;
     }
