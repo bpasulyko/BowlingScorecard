91bddca3b875efe432c5934480cf0cdcad192ba7
bpasulyko <bpasulyko@gmail.com>
2016-11-19 10:21:32 -0600

    Added the home screen to the app

---
+++ app/src/main/java/bpasulyko/bowlingscorecard/HomeScreen.java
@@ -0,0 +1,38 @@
+package bpasulyko.bowlingscorecard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        createToolbar();
+    }
+
+    private void createToolbar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(myToolbar);
+    }
+
+    public void viewStats(View view) {
+        Intent intent = new Intent(this, Stats.class);
+        startActivity(intent);
+    }
+
+    public void viewAllGames(View view) {
+        Intent intent = new Intent(this, GamesList.class);
+        startActivity(intent);
+    }
+
+    public void addGames(View view) {
+        Intent intent = new Intent(this, AddScores.class);
+        startActivity(intent);
+    }
+}
