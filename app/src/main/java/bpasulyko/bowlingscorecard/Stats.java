f34dcac9a73ba6474db17dbebd118c91dd0876e5
bpasulyko <bpasulyko@gmail.com>
2016-11-27 16:20:12 -0600

    Added a scorecard selector to the statistics page

--- app/src/main/java/bpasulyko/bowlingscorecard/Stats.java
+++ app/src/main/java/bpasulyko/bowlingscorecard/Stats.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,40 +15,64 @@ import java.util.List;
 
 import bpasulyko.bowlingscorecard.dbHandlers.MainDbHandler;
 import bpasulyko.bowlingscorecard.models.Game;
+import bpasulyko.bowlingscorecard.models.ScoreCard;
 
 public class Stats extends AppCompatActivity {
 
+    private MainDbHandler dbHandler;
     private List<Game> allGames;
-    List<Double> allScores = new ArrayList<>();
+    private List<Double> allScores;
+    private TextView runningAvg;
+    TextView bestAvg;
+    TextView bestGame;
+    TextView worstGame;
+    TextView best3GameTotal;
+    TextView threeGameAverage;
+    TextView totalGames;
+    TextView gamesOverAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        dbHandler = new MainDbHandler(this, null, null, 1);
+        runningAvg = (TextView) findViewById(R.id.runningAvg);
+        bestAvg = (TextView) findViewById(R.id.bestAvg);
+        bestGame = (TextView) findViewById(R.id.bestGame);
+        worstGame = (TextView) findViewById(R.id.worstGame);
+        best3GameTotal = (TextView) findViewById(R.id.best3GameTotal);
+        threeGameAverage = (TextView) findViewById(R.id.threeGameAverage);
+        totalGames = (TextView) findViewById(R.id.numberOfGames);
+        gamesOverAverage = (TextView) findViewById(R.id.gamesOverAvgerage);
+
         createToolbar();
-        MainDbHandler dbHandler = new MainDbHandler(this, null, null, 1);
-        allGames = dbHandler.getAllGames(1);
+        createSpinner();
+        populateGamesAndScores(null);
+    }
+
+    private void populateGamesAndScores(Integer scorecardId) {
+        allGames = dbHandler.getAllGames(scorecardId);
+        allScores = new ArrayList<>();
         for (Game game : allGames) {
             allScores.addAll(game.getScores());
         }
 
-        TextView runningAvg = (TextView) findViewById(R.id.runningAvg);
-        TextView bestAvg = (TextView) findViewById(R.id.bestAvg);
-        TextView bestGame = (TextView) findViewById(R.id.bestGame);
-        TextView worstGame = (TextView) findViewById(R.id.worstGame);
-        TextView best3GameTotal = (TextView) findViewById(R.id.best3GameTotal);
-        TextView threeGameAverage = (TextView) findViewById(R.id.threeGameAverage);
-        TextView totalGames = (TextView) findViewById(R.id.numberOfGames);
-        TextView gamesOverAverage = (TextView) findViewById(R.id.gamesOverAvgerage);
-
-        runningAvg.setText(formatNumber(getRunningAverage()));
-        bestAvg.setText(formatNumber(getBestAverage()));
-        bestGame.setText(formatNumber(getBestGame()));
-        worstGame.setText(formatNumber(getWorstGame()));
-        best3GameTotal.setText(formatNumber(getBest3GameTotal()));
-        threeGameAverage.setText(formatNumber(get3GameAverage()));
-        totalGames.setText(formatNumber((double) allScores.size()));
-        gamesOverAverage.setText(String.format("%s%%", formatNumber(getPercentageOfGamesAboveAverage())));
+        String runningAvg = (allGames.size() > 0) ? formatNumber(getRunningAverage()) : "--";
+        String bestAvg = (allGames.size() > 0) ? formatNumber(getBestAverage()) : "--";
+        String bestGame = (allGames.size() > 0) ? formatNumber(getBestGame()) : "--";
+        String worstGame = (allGames.size() > 0) ? formatNumber(getWorstGame()) : "--";
+        String best3GameTotal = (allGames.size() > 0) ? formatNumber(getBest3GameTotal()) : "--";
+        String threeGameAverage = (allGames.size() > 0) ? formatNumber(get3GameAverage()) : "--";
+        String totalGames = (allGames.size() > 0) ? formatNumber((double) allScores.size()) : "--";
+        String gamesOverAverage = (allGames.size() > 0) ? String.format("%s%%", formatNumber(getPercentageOfGamesAboveAverage())) : "--";
+        this.runningAvg.setText(runningAvg);
+        this.bestAvg.setText(bestAvg);
+        this.bestGame.setText(bestGame);
+        this.worstGame.setText(worstGame);
+        this.best3GameTotal.setText(best3GameTotal);
+        this.threeGameAverage.setText(threeGameAverage);
+        this.totalGames.setText(totalGames);
+        this.gamesOverAverage.setText(gamesOverAverage);
     }
 
     private Double getRunningAverage() {
@@ -122,9 +150,35 @@ public class Stats extends AppCompatActivity {
     private void createToolbar() {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitle(R.string.statistics);
+        myToolbar.setElevation(0);
         setSupportActionBar(myToolbar);
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
     }
+
+    private void createSpinner() {
+        ScoreCard allOption = new ScoreCard(null, "All");
+        List<ScoreCard> scorecards = new ArrayList<>();
+        scorecards.add(allOption);
+        scorecards.addAll(dbHandler.getAllScorecards());
+
+        Spinner spinner = (Spinner) findViewById(R.id.scorecardsDropdown);
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, scorecards);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(itemSelectedListener);
+    }
+
+    private AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            ScoreCard scorecard = (ScoreCard) parent.getItemAtPosition(position);
+            populateGamesAndScores(scorecard.getId());
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {}
+    };
 }
